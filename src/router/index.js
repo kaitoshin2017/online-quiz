11a6b406ae9991a4d3031aedeb9f2ce79bb5512d
rantpogi <altaquekylacezar@gmail.com>
2025-02-27 14:40:12 +0800

    yhyh
    gbyhbgjhn

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: () => import('../views/RegistrationView.vue'),
+    }
   ],
 })
 
