1d37b72d018f2f127fc3b6f331f62985d82557f6
KupalKa22 <christopherpolintan22@gmail.com>
2025-02-27 15:59:08 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="#">MyWebsite</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div class="menu-icon" id="menu-icon">
+                <i class="fas fa-bars"></i>
+            </div>
+        </nav>
 </template>
+
+<style>
+
+</style>
+
+<scri>
+
+  
+</scri>
