57561727c78ceb38477bf43e18ca77306b078982
KupalKa22 <christopherpolintan22@gmail.com>
2025-03-11 13:36:19 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,115 +1,125 @@
 <template>
+  <div id="app">
     <nav class="navbar">
       <!-- Logo -->
       <div class="logo">
         <a href="#">Online Quiz</a>
       </div>
-      
+
       <!-- Navbar Links -->
-      <ul class="nav-links">
+      <ul class="nav-links" :class="{ active: menuActive }">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">FAQ</a></li>
         <li><a href="#">Login</a></li>
         <li><a href="#">Sign up</a></li>
+
       </ul>
-      
+
       <!-- Hamburger Menu Icon -->
-      <div class="burger" id="burger">
+      <div class="burger" @click="toggleMenu">
         <div class="line"></div>
         <div class="line"></div>
         <div class="line"></div>
       </div>
     </nav>
+  </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      menuActive: false, // To toggle the menu on mobile
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuActive = !this.menuActive; // Toggle active class
+    },
+  },
+};
+</script>
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+<style scoped>
 
 
 .navbar {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
-  padding-left: 20%;
-  position: fixed;
-  top: 0;
-  width: 80%;
+  background-color: #313131;
+  height: 70px;
+  padding: 10px 20px;
 }
 
+
 .navbar .logo a {
-  color: white;
+  color: rgb(255, 255, 255);
   font-size: 24px;
+  font-weight: bold;
   text-decoration: none;
 }
 
-.nav-links {
+
+.navbar .nav-links {
   list-style: none;
   display: flex;
 }
 
-.nav-links li {
-  margin: 0 20px;
+.navbar .nav-links li {
+  margin: 0 15px;
 }
 
-.nav-links a {
+.navbar .nav-links li a {
   color: white;
   text-decoration: none;
   font-size: 18px;
-  padding: 10px;
-  transition: 0.3s;
 }
 
-.nav-links a:hover {
-  background-color: #555;
-  border-radius: 4px;
+.navbar .nav-links li a:hover {
+  color: cyan;
 }
 
-/* Hamburger Menu */
 .burger {
   display: none;
-  cursor: pointer;
   flex-direction: column;
   justify-content: space-between;
   width: 30px;
   height: 21px;
+  cursor: pointer;
 }
 
 .burger .line {
-  height: 3px;
   width: 100%;
+  height: 3px;
   background-color: white;
 }
 
-@media (max-width: 768px) {
-  .nav-links {
+@media screen and (max-width: 768px) {
+  .navbar .nav-links {
     display: none;
     width: 100%;
+    flex-direction: column;
     position: absolute;
-    top: 60px;
+    top: 50px;
     left: 0;
     background-color: #333;
-    flex-direction: column;
-    align-items: center;
+    text-align: center;
   }
 
-  .nav-links.active {
-    display: flex;
-  }
-
-  .nav-links li {
-    margin: 10px 0;
+  .navbar .nav-links li {
+    margin: 15px 0;
   }
 
   .burger {
     display: flex;
   }
-}
 
+
+  .nav-links.active {
+    display: flex;
+  }
+}
 </style>
